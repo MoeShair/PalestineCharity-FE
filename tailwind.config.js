bde21b1063da847aaa101821d7b4969ba7b195af
MoeShair <hamoda.tam@gmail.com>
2024-03-02 14:12:34 +0200

    added major components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: {
+          300: '#bf67cf',
+          400: '#b144c4',
+          500: '#a21fba',
+          600: '#941db4',
+        },
+      },
+    },
   },
   plugins: [],
 }
