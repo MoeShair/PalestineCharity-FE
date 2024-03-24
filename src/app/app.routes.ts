8cc7c624be58728e9b9769132d526fd7477287bc
MoeShair <hamoda.tam@gmail.com>
2024-03-24 15:15:58 +0200

    side nav structure code is ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,18 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/shop/shop.component').then(m => m.ShopComponent)
   },
   {
+    path: 'aboutus',
+    loadComponent: () => import('./pages/aboutus/aboutus.component').then(m => m.AboutusComponent)
+  },
+  {
+    path: 'item',
+    loadComponent: () => import('./pages/item/item.component').then(m => m.ItemComponent)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent)
+  },
+  {
     path: '',
     loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
   }
